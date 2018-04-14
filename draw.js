a2f61794e4ec0ea8f636d583abbe572c6dd7d9f1
Zack Stout <zackstout@gmail.com>
2018-04-13 19:07:57 -0500

    seems to be mostly working, just waves are happening too often

--- draw.js
+++ draw.js
@@ -1,6 +1,7 @@
 
-var waves = [];
-
+let waves = [];
+let planes = [];
+let i = 0;
 
 function setup() {
   createCanvas(800, 500);
@@ -12,11 +13,46 @@ function draw() {
   waves.forEach(wave => {
     wave.expand();
   });
+  planes.forEach(plane => {
+    if (i % 10 == 0) {
+      plane.makeWave();
+    }
+    i++;
+    plane.move();
+    plane.sketch();
+  });
 }
 
 function mouseClicked() {
-  const wave = new Wave(mouseX, mouseY);
-  waves.push(wave);
+  // const wave = new Wave(mouseX, mouseY);
+  // waves.push(wave);
+  const plane = new Plane(mouseX, mouseY);
+  // plane.start();
+  planes.push(plane);
+}
+
+class Plane {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  makeWave() {
+    const wave = new Wave(this.x, this.y);
+    waves.push(wave);
+  }
+
+  // start() {
+  //   setInterval(this.makeWave, 300);
+  // }
+
+  sketch() {
+    ellipse(this.x, this.y, 3, 3);
+  }
+
+  move() {
+    this.x += 1;
+  }
 }
 
 class Wave {
